2a1d0cebe8ad9e559539d2e8e1d024eb114e6c89
kunal528 <64953335+kunal528@users.noreply.github.com>
2022-03-03 22:14:59 +0530

    merge conflicts solved

--- components/NavBar/Navbar.js
+++ components/NavBar/Navbar.js
@@ -4,11 +4,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import { useState } from "react";
 import { FiPlusCircle, FiBell, FiUser } from 'react-icons/fi';
 
-<<<<<<< HEAD
-const NavBar = ({route, plusClick, bellClick, userClick}) => {
-=======
 const NavBar = () => {
->>>>>>> f6f1a782ddb0032ed344090fb6c23537f0839a1e
 
   const [search, setSearch] = useState("")
 
@@ -18,18 +14,6 @@ const NavBar = () => {
     setSearch("");
   }
 
-<<<<<<< HEAD
-  return(
-    <div className = {navbarstyle.base}>
-    <div className = {navbarstyle.search}>    
-        <SearchBar r = "40px" onClick = {() => Search()} value = {search} setSearch = {setSearch}/></div>
-    <div className = {navbarstyle.groupIcons}>
-        <FiPlusCircle className = {navbarstyle.pluscircle} onClick = {plusClick}/>
-        <FiBell className = {navbarstyle.bell} onClick = {bellClick}/>
-        <div className = {navbarstyle.userclass}>
-        <FiUser className = {navbarstyle.user} onClick = {userClick}/></div>
-    </div>
-=======
   return (
     <div className={navbarstyle.base}>
       <div className={navbarstyle.search}>
@@ -39,7 +23,6 @@ const NavBar = () => {
       <div className={navbarstyle.userclass}>
         <FiUser className={navbarstyle.user} />
       </div>
->>>>>>> f6f1a782ddb0032ed344090fb6c23537f0839a1e
     </div>
   )
 }
