3b35dccc77a23d99553977746df52fefa5f38347
kunal528 <64953335+kunal528@users.noreply.github.com>
2022-03-03 19:25:10 +0530

    navbar position sticky

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-     <div style = {{ height: "100%", color: "black"}}>
-      Hello
+    <div style={{ height: "100%", color: "black" }}>
+      Home
     </div>
   )
 }
