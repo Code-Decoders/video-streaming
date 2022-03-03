b4604b4aa8391cb17be557b996a7cd49f8e1f209
Ranaco <ranasatyamraj@gmail.com>
2022-03-03 22:31:03 +0530

    finished with the video tile

--- components/VideoTile/videoTile.js
+++ components/VideoTile/videoTile.js
@@ -8,7 +8,7 @@ const VideoTile = ({image, title, player, game}) => {
         <div className = {styles.videoimage}>
         <Image borderRadius = "20px" objectFit = "cover" src = {image} alt = "image tile"/>       
         </div>
-        <div style = {{ fontSize: "25px" }}>
+        <div style = {{ fontSize: "25px",paddingTop: "5px" }}>
         {title}
         </div>
       <div style = {{ fontSize: "15px", paddingTop: "5px" }}>
