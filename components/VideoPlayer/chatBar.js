0bb9cf4ff368b2000c2754febca60e53483490fc
Ranaco <ranasatyamraj@gmail.com>
2022-03-10 18:00:39 +0530

    Chat bar done

--- components/VideoPlayer/chatBar.js
+++ components/VideoPlayer/chatBar.js
@@ -8,11 +8,21 @@ const SideBar = () => {
 
   return(
     <div className = {styles['chat-bar-base']} style = {isOpen ? { width: "80%"}: {width: "10%"}}>
-          <div onClick = {() => {setIsOpen(!isOpen)}} className = {styles['chat-bar-button']}>
+    <div style = {{ display: "flex",
+        paddingLeft: "15px",
+        justifyContent: "start",
+        alignItems: "center",
+        gap: "13%",
+        height: "65px",
+}}>
+        <div onClick = {() => {setIsOpen(!isOpen)}} className = {styles['chat-bar-button']}>
             {isOpen ? <FiChevronRight style = {{ height: "20px", width: "25px"}}/>
-              : <FiChevronLeft style = {{ height: "20px", width: "25px"}}/>}
-          </div>
-
+              : <FiChevronLeft style = {{ height: "20px", width: "25px"}}/>}       
+    </div>
+      { isOpen ? <div style = {{animationDelay: "0.4s",justifySelf: "center", color: "white", fontSize: "20px"}}>
+          Stream Chat
+      </div> : <></>}
+    </div>
     </div>
   )
 }
