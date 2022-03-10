311247d1a82e60a3c93623e4e3451e4797f3f408
kunal528 <64953335+kunal528@users.noreply.github.com>
2022-03-10 15:19:08 +0530

    login page updated

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import styles from '../../styles/Login.module.css'
 import Layout from '../../components/layouts/secondary'
 import Image from 'next/image'
@@ -6,76 +6,77 @@ import Illustration from '../../public/images/login_illustration.png'
 import MetaMask from '../../public/images/metamask-logo-png-transparent 1.png'
 
 
- const Login = () => {
+const Login = () => {
 
-    const [authInstance, setAuthInstance] = useState(null);
+  const [authInstance, setAuthInstance] = useState(null);
 
-    const getAuth = async () => {
-      const AuthProvider = (await import("@arcana/auth")).AuthProvider;
-      AuthProvider.handleRedirectPage(window.location);
-      const Auth = new AuthProvider({
-        appID: '568',
-        network: "testnet",
-        oauthCreds: [
-          {
-            type: "google",
-            clientId: "194404779871-s8hde43bkdc0du6afi37na3g6hn9h4kh.apps.googleusercontent.com",
-          },
-        ],
-        redirectUri: "http://localhost:3000/",
-      })
-      setAuthInstance(Auth);
-    }
+  const getAuth = async () => {
+    const AuthProvider = (await import("@arcana/auth")).AuthProvider;
+    AuthProvider.handleRedirectPage(window.location);
+    const Auth = new AuthProvider({
+      appID: '568',
+      network: "testnet",
+      oauthCreds: [
+        {
+          type: "google",
+          clientId: "194404779871-s8hde43bkdc0du6afi37na3g6hn9h4kh.apps.googleusercontent.com",
+        },
+      ],
+      redirectUri: 'http://localhost:3000/',
+    })
+    setAuthInstance(Auth);
+  }
 
-    useEffect( () => {
-       getAuth()
-    }, [])
+  useEffect(() => {
+    getAuth()
+  }, [])
 
-    const login = async () => {
-      await authInstance.loginWithSocial("google");
-    }
+  const login = async () => {
+    await authInstance.loginWithSocial("google");
+  }
 
-    console.log(authInstance)
+  console.log(authInstance)
 
-    return <div> <div className = {styles.base}>
-    <div style = {{paddingTop: "180px", display: "flex", flexDirection: "column",alignItems: "space-around", height: "100vh", gap: "100px"}}>
-      <div className = {styles.bannerTextStyle}>
+  return <div className={styles.base}>
+    <div className={styles.leftPane}>
+      <h1 className={styles.bannerTextStyle}>
         Login with your web3 wallet!
+      </h1>
+      <img src={'https://dashboard.arcana.network/assets/sidebar-illustration.ea9e51ec.png'} alt="" className={styles['login_illustration']} />
     </div>
-    <div  className = {styles['login_illustration']} >
-    <Image src = {Illustration} alt = ""/>
+    <div className={styles.dividerBar} />
+    <div style={{ display: 'flex', flex: '1', justifyContent: 'center', alignItems: 'center' }}>
+      <div className={styles.rightPane}>
+        <div>
+          <div style={{ color: "white", fontSize: "70px",fontWeight: 'bolder' }}>
+            Welcome to Gamoly
+          </div>
+          <div style={{ fontSize: "24px", textAlign: 'center' }}>
+            {"Let's get started"}
+          </div>
+        </div>
+        <div className={styles['continue']}>
+          Continue with:
+        </div>
+        <div onClick={login} className={styles['metamask-button']} >
+          <Image src={MetaMask} alt="metamask logo" />
+          <div>
+            Metamask
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    <div style = {{ height: "100vh", width: "5px", backgroundColor: "black"}}>
-    </div>
-    <div className = {styles.rightPane}>
-            <div style = {{color: "white", fontSize: "70px", width: "620px"}}>
-              Welcome to Video Player
-            </div>
-            <div style = {{ fontSize: "24px"}}>
-              Let&apos;s get started
-            </div>
-            <div className = {styles['continue']}>
-              Continue with:
-            </div>
-            <div onClick={login} className = {styles['metamask-button']} >
-                <Image src = {MetaMask} alt = "metamask logo" />
-                <div>
-                    Metamask
-                </div>
-            </div>
-    </div>
-    </div></div>
-        }
+  </div>
+}
 
-      
+
 Login.getLayout = (page) => {
-  
-          return(
-            <Layout title = {"Login"}>
-            {page}
-            </Layout>
-          )
-        }
-        
-  export default Login
+
+  return (
+    <Layout title={"Login"}>
+      {page}
+    </Layout>
+  )
+}
+
+export default Login
